0bc36e96c02cff91048b4640b3d9f6158c42f340
aabccd021 <aabccd021@gmail.com>
2023-10-26 19:45:17 +0700

    Refactor init.sh script and remove unnecessary echo statements

--- init.sh
+++ init.sh
@@ -33,15 +33,10 @@ ssh "$user_host" "
   ./config.sh --url $repo_url --token $token --name $runner_name --replace --unattended
 "
 
-echo "Runner $runner_name for $repo_url is now configured on $user_host"
-
 ssh "$root_host" "
   cd /home/$user_host_name/runners/$repo_dirname
-  echo "installing service"
   ./svc.sh install
-  echo "starting service"
   ./svc.sh start
-  echo "service started"
 " 
 
 echo "Done! Runner $runner_name for $repo_url is now running on $user_host"
